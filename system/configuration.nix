f99c304e3e9265d9610d035de3cc189ef6e4f2d9
llity <nexthere@hotmail.com>
2023-02-17 20:05:34 +0800

    fix network error

--- system/configuration.nix
+++ system/configuration.nix
@@ -50,7 +50,7 @@ in
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
-    useDHCP = true;
+    useDHCP = false;
   };
 
   # Configure network proxy if necessary
