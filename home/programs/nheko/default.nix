1bafb1fc6c8540ada55c1ea4a9abcf2e4cab471d
llity <nexthere@hotmail.com>
2023-02-17 20:56:31 +0800

    modified substituters

--- home/programs/nheko/default.nix
+++ home/programs/nheko/default.nix
@@ -21,7 +21,7 @@ in
         accessToken = token;
         deviceId = "CGDVEGHPLX";
         homeServer = "https://matrix-client.matrix.org:443";
-        userId = "@@llity:matrix.org";
+        userId = "@@gvolpe:matrix.org";
       };
 
       sidebar = {
--- system/cachix/gvolpe-nixos.nix
+++ system/cachix/gvolpe-nixos.nix
@@ -1,11 +1,11 @@
 {
   nix.settings = {
     substituters = [
-      "https://llity-nixos.cachix.org"
+      "https://gvolpe-nixos.cachix.org"
       "https://cache.garnix.io"
     ];
     trusted-public-keys = [
-      "llity-nixos.cachix.org-1:0MPlBIMwYmrNqoEaYTox15Ds2t1+3R+6Ycj0hZWMcL0="
+      "gvolpe-nixos.cachix.org-1:0MPlBIMwYmrNqoEaYTox15Ds2t1+3R+6Ycj0hZWMcL0="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
